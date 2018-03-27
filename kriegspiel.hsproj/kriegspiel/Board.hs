e95855c003a2df6af3e726fbf8e13fb5eb2bfce3
Eric Zoerner <eric.zoerner@gmail.com>
2018-03-27 07:15:09 -0700

    code cleanup

--- kriegspiel.hsproj/kriegspiel/Board.hs
+++ kriegspiel.hsproj/kriegspiel/Board.hs
@@ -1,22 +1,20 @@
 module Board where
-  
-import Linear.V2 (V2(V2))
+
+import Foundation (ifThenElse)  
 import Helm.Graphics2D
 import Helm.Asset
 import Helm.Engine (Engine)
+import Linear.V2 (V2(V2))
 
 form :: Engine e => Image e -> Image e -> Int -> Form e
 form lightSquare darkSquare boardSide = let
     squareSide = fromIntegral boardSide / 8
-    imageDims = (V2 squareSide squareSide)
-    chooseImage x y = if (x + y) `mod` 2 == 0
-                        then lightSquare
-                        else darkSquare
+    imageDims = V2 squareSide squareSide
+    chooseImage x y = ifThenElse ((x + y) `mod` 2 == 0) lightSquare darkSquare
     mkForm x y = image imageDims $ chooseImage x y
   in
-    toForm $ collage [move (V2 h v) (mkForm (floor x) (floor y)) |
+    toForm $ collage [move (V2 h v) $ mkForm (floor x) $ floor y |
                          x <- [0..7]
                        , y <- [0..7]
                        , let h = x * squareSide
-                       , let v = y * squareSide]
-                       
+                       , let v = y * squareSide]                 
